98c6adf36ee7700a4840513f3a5d17d4b7cad981
Arvind Ravulavaru <arvind.ravulavaru@gmail.com>
2014-09-24 23:46:51 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -14,7 +14,11 @@ app.all('/*', function(req, res, next) {
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
   // Set custom headers for CORS
   res.header('Access-Control-Allow-Headers', 'Content-type,Accept,X-Access-Token,X-Key');
-  next();
+  if (req.method == 'OPTIONS') {
+    res.status(200).end();
+  } else {
+    next();
+  }
 });
 
 // Auth Middleware - This will check if the token is valid
